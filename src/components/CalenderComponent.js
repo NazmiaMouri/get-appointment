3e08942131d6e21e069c43985897010cca5876d6
Nazmia Mouri <edelsteine025@gmail.com>
2020-08-28 23:50:23 +0600

    'scheduleHalfSet'

--- src/components/CalenderComponent.js
+++ src/components/CalenderComponent.js
@@ -1,78 +1,115 @@
 import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap'
+import { Breadcrumb, BreadcrumbItem,Modal,ModalBody,ModalHeader,Form,FormGroup,Label,Input,Button } from 'reactstrap'
 import { Link } from 'react-router-dom';
-import { Inject,ViewDirective,ViewsDirective,ScheduleComponent,Week,Month,EventSettingsModel} from '@syncfusion/ej2-react-schedule'
+import { Inject,ViewDirective,ViewsDirective,ScheduleComponent,Week} from '@syncfusion/ej2-react-schedule'
+import moment from 'moment';
 
 class Calender extends React.Component {
  
-    //  const doctor=props.doctor
-    //  var slotPerDay= 60/doctor.visitDurationInMin
-
-     
-
-    // console.log(typeof(Day))
-    // console.log(Day[0],Day[1])
-    // console.log(Day.length)
+    
     //  "availibility":{
     //     "sun":"10:00 AM - 06:00 PM",
     //     "mon":"09:00 PM - 11:00 PM",
     //     "thu":"11:00 AM - 02:00 PM"
     //     },
    getWorkCellText(date) {
-    // function showDay(obj,objName){
-
+   
       var i=0;
       let weekEnds = [];
-      
+      let startTime = [];
+      let endTime = [];
+      let splitted=[];
+      let slotCount= 60 / this.props.doctor.visitDurationInMin;
+      let boxHeight= (144 /slotCount) - 7;
 
       for( var day in this.props.doctor.availibility){
           if(this.props.doctor.availibility.hasOwnProperty(day)){
+               var time = this.props.doctor.availibility[day]
+               
                 switch(day){
 
                   case "sun":{
                     weekEnds[i]=0;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                   
                     i++;
                     break;
                 }
                   case "mon":{
                     weekEnds[i]=1;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                   
                     i++;
                   break;
               }
                   case "tue":{
                     weekEnds[i]=2;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                   
                     i++;
                   break;
               }
                   case "wed":{
                     weekEnds[i]=3;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                    
                     i++;
                   break;
               }
                   case "thu":{
                     weekEnds[i]=4;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                    
                     i++;
                   break;
               }
                   case "fri":{
                     weekEnds[i]=5;
+                    splitted=time.split("-")
+                    startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                    endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                  
                     i++;
                   break;
               }
               case "sat":{
                 weekEnds[i]=6;
+                splitted=time.split("-")
+                startTime[i]=parseInt(moment(splitted[0],["h:mm A"]).format("HH:mm"))
+                endTime[i]=parseInt(moment(splitted[1],["h:mm A"]).format("HH:mm"))
+                   
                 i++;
               break;
-            }
-            default: 
-               break;
+              }
+              default: 
+                break;
+           }
       
           }
       }
-      }
-      if (weekEnds.indexOf(date.getDay()) >= 0) {
-          return "<i >Available</i>";
-      }
+      
+    
+
+    
+      if (weekEnds.indexOf(date.getDay()) >= 0 ){
+        if(startTime[weekEnds.indexOf(date.getDay())] <= date.getHours() && date.getHours() <= endTime[weekEnds.indexOf(date.getDay())]){
+     
+        return `<a href='#form'><button  class='btn'  '><i class='fa fa-check floral-white'style='color:green'></i></button></a>`;
+          }
+        
+        
+        }
+        console.log("BOXXXXXXX"+ boxHeight)
       return '';
   }
   ;
@@ -85,7 +122,9 @@ class Calender extends React.Component {
   }
   render(){
       return (
-            <div className='container'>
+      //-----------------------------------Container START-------------------------------------------// 
+      // ---------------------------------------Breadcrumb------------------------------------------//
+            <div className='container '>
                     <div className="row">
                      <Breadcrumb>
                             <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
@@ -96,17 +135,40 @@ class Calender extends React.Component {
                             <hr />
                         </div>                
                     </div>
-                   
-
-            <ScheduleComponent  cellTemplate={this.cellTemplate.bind(this)}>
-              <ViewsDirective>
-                
-                <ViewDirective option='Week' startHour='08:00' endHour='22:00' timeScale={{ enable: true, slotCount: 60/this.props.doctor.visitDurationInMin }}/>
-              
-                <ViewDirective option='Month'/>
-              </ViewsDirective>
-            <Inject services={[Week, Month]}/>
-            </ScheduleComponent>
+    {/*---------------------------------CALENDER VIEW --------------------------------------------------  */}
+                <div className='mb-10'>
+                    <ScheduleComponent  height='500px'cellTemplate={this.cellTemplate.bind(this)}>
+                      <ViewsDirective>
+                        
+                        <ViewDirective option='Week' startHour='08:00' endHour='25:00' timeScale={{ enable: true, slotCount: 60/this.props.doctor.visitDurationInMin }}/>
+                      
+                        
+                      </ViewsDirective>
+                    <Inject services={[Week]}/>
+                    </ScheduleComponent>
+                </div>
+  {/* -----------------------------------FORM MODAL--------------------------------------------------- */}
+                 <Modal id='form'>
+                <ModalHeader>Add Details</ModalHeader>
+                <ModalBody>
+                    <Form >
+                        <FormGroup>
+                            <Label htmlFor='username'>Full Name</Label>
+                            <Input type='text' id='username' name='username' innerRef={(input)=>this.username=input}></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor='age'>Age</Label>
+                            <Input type='age' id='age' name='age' innerRef={(input)=>this.password=input}></Input>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label htmlFor='gender'>Age</Label>
+                            <Input type='gender' id='gender' name='gender' innerRef={(input)=>this.password=input}></Input>
+                        </FormGroup>
+                        
+                        <Button type='submit' value='submit' className='primary' >Book</Button>
+                    </Form>
+                </ModalBody>
+            </Modal> 
             </div>
   )
   }
@@ -115,32 +177,7 @@ class Calender extends React.Component {
 
     
    
-    //     return (
-    //         <div className='container'>
-    //             <div className="row">
-    //                 <Breadcrumb>
-    //                     <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-    //                     <BreadcrumbItem active>Schedule</BreadcrumbItem>
-    //                 </Breadcrumb>
-    //                 <div className="col-12">
-    //                     <h3>Schedule of {doctor.name}</h3>
-    //                     <hr />
-    //                 </div>                
-    //             </div>
-    //                 <ScheduleComponent   eventSettings={Day}>
-    //                     <ViewsDirective>
-            
-    //                         <ViewDirective option='Week' startHour='08:00' endHour='22:00' timeScale={{ enable: true, slotCount: slotPerDay  }} />
-    //                         <ViewDirective option='Month' />
-                        
-    //                     </ViewsDirective>
-    //                     <Inject services={[ Week, Month]}/>
-    //                 </ScheduleComponent>
-
-    //         </div>
-       
-    //     )
-    // }
+   
 
 export default Calender;
 
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -11,7 +11,7 @@ function Header () {
             <div className='row row-header'>
                 <div className='col-12 col-sm-8'>
                     <h1>Doctors' Appointment</h1>
-                    <p>With the use of technology, we aim to do just that; connect and bridge caregivers and the patients so that the people can live better and healthier lives.</p>
+                    <p>We seek to link and bridge caregivers and patients through the use of technology so that people can live happier and healthier lives.</p>
                 </div>
                 <div className='col-12 col-sm-4'>
                  <img src='/assets/stuff.png' className='img-fluid rounded-circle' alt='logo'/>
