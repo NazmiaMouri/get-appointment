dea5a015ef004859236e17899b8557b7f46f02a8
Nazmia Mouri <edelsteine025@gmail.com>
2020-08-30 23:52:31 +0600

    'SearchBar'

--- src/components/DoctorComponents.js
+++ src/components/DoctorComponents.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Media} from 'reactstrap'
 import { Link} from 'react-router-dom';
 
+
+
 function Doctor (props){
     
+    const[ inputsearch,setInputSearch]= useState(null);
+
+    function onchange(event){
+       let value=event.target.value
+       setInputSearch(value);
+      
+    }
     
-    const show_doctors=props.doctors.map((doctor) => {
+
+
+    const show_doctors=props.doctors.filter((doctor) => {
+        if(inputsearch == null){
+          return doctor
+        }
+        else if ((doctor.name).toLowerCase().includes(inputsearch.toLowerCase()) || (doctor.org).toLowerCase().includes(inputsearch.toLowerCase()) || (doctor.specialization).toLowerCase().includes(inputsearch.toLowerCase())){
+          return doctor
+      }
+     
+    }).map((doctor) => {
         return (
             <div  className='col-12 mt-5'>
                <Link to={`/home/${doctor.id}`} >
@@ -32,9 +51,12 @@ function Doctor (props){
 return(
 
     <div className='container'>
+        <div className="row justify-content-center m-auto">
+           <input  classname="col-10 col-sm-6" type="text" id="myInput"  placeholder="Search for names, Hospital names, Specilizations.."  value={inputsearch} onChange={onchange}/> 
+        </div>
         <div className="row row-content">
                     
-                    <div className="col-12">
+                    <div className="col-12 col-sm-10">
                     <h2>Our Specialists</h2>
                         <Media list>
                         {show_doctors}
